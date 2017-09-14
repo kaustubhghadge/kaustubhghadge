066a586bf438e74485aa3c4f449973d9824dc7eb
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2017-09-14 13:56:07 -0500

    matrix

--- js/main.js
+++ js/main.js
@@ -65,5 +65,61 @@ $(document).ready(function () {
             }
 
         });
-    });
+//matrix code
+
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+//making the canvas full screen
+c.height = window.innerHeight - 40;
+c.width = window.innerWidth - 40;
+
+//chinese characters - taken from the unicode charset
+var matrixCode = "田K由A甲U申S甴T电U甶B男H甸.甹G町H画A甼D甽G甾E甿@畀g畁m畂a畃i畄l畅.畆c畇o畈m畉γνῶθι.σεαυτόν.nosce.te.ipsum.temet.nosce";
+//converting the string into an array of single characters
+matrixCode = matrixCode.split("");
+
+var font_size = 10;
+var columns = c.width/font_size; //number of columns for the rain
+//an array of drops - one per column
+var drops = [];
+//x below is the x coordinate
+//1 = y co-ordinate of the drop(same for every drop initially)
+for(var x = 0; x < columns; x++)
+    drops[x] = 1; 
+
+//drawing the characters
+function draw()
+{
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, c.width, c.height);
+    
+    ctx.fillStyle = "#0F0"; //green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for(var i = 0; i < drops.length; i++)
+    {
+        //a random matrixCode character to print
+        var text = matrixCode[Math.floor(Math.random()*matrixCode.length)];
+        //x = i*font_size, y = value of drops[i]*font_size
+        ctx.fillText(text, i*font_size, drops[i]*font_size);
+        
+        //sending the drop back to the top randomly after it has crossed the screen
+        //adding a randomness to the reset to make the drops scattered on the Y axis
+        if(drops[i]*font_size > c.height && Math.random() > 0.975)
+            drops[i] = 0;
+        
+        //incrementing Y coordinate
+        drops[i]++;
+    }
+}  
+ function matrix(){
+    var $konsole=$("#konsole").remove();
+    $("#c").removeClass("hide");
+    setInterval(draw, 40);
+}
+$('.username').on('click',matrix);
+});
 });
