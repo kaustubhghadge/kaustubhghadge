6358b5e4acf5609f1ad3100239d33a66c5b80db8
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2020-08-24 14:50:16 -0500

    testing basic blog

---
+++ blog/blog.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $(function () {
+
+$('.blog_index').typed({
+    strings: [" ^3000 ls posts"],
+    typeSpeed: 0,
+    showCursor: true,
+    contentType: 'text',
+    startDelay: 10,
+    callback: function () {
+        setTimeout(function () {
+            $("#posts").removeClass("hide");
+            $("#ls").find(".typed-cursor").hide();
+        }, 3000);
+    },
+    preStringTyped: function () {
+        setTimeout(function () {
+            $("#ls").removeClass("hide");
+        }, 700);
+    }
+});
+
+
+})});
+
+
+
